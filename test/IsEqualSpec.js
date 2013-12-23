23ce4d29de0de4669835e8669d7ee7e8c98c10f3
Raphael Luba <raphael@leanbyte.com>
2013-12-23 23:03:02 +0100

    IsEqual: Add adapters to convert values to matchers

--- test/IsEqualSpec.js
+++ test/IsEqualSpec.js
@@ -1,49 +1,106 @@
 'use strict';
 
-var equalTo = require('../lib/matchers/IsEqual').equalTo
+var IsEqual = require('../lib/matchers/IsEqual')
 	, Description = require('../lib/Description')
+	, hamjest = require('../lib/hamjest')
 	, assertTrue = require('./asserts').assertTrue
 	, assertFalse = require('./asserts').assertFalse
 	, assertEquals = require('./asserts').assertEquals
+	, TestMatcher = require('./TestMatcher')
 	;
 
 describe('IsEqual', function () {
 
-	it('should match same strings', function () {
-		assertTrue(equalTo('a string').matches('a string'));
+	describe('equalTo', function () {
+		var equalTo = IsEqual.equalTo;
+
+		it('should match same strings', function () {
+			assertTrue(equalTo('a string').matches('a string'));
+		});
+
+		it('should not match different strings', function () {
+			assertFalse(equalTo('a string').matches('another string'));
+		});
+
+		it('should not match strings and numbers', function () {
+			assertFalse(equalTo('2').matches(2));
+		});
+
+		it('should match different but equivalent objects', function () {
+			assertTrue(equalTo({a: 1, b: 2}).matches({a: 1, b: 2}));
+		});
+
+		it('should not match unequivalent objects', function () {
+			assertFalse(equalTo({a: 1, b: 2}).matches({a: 1, b: 3}));
+			assertFalse(equalTo({a: 1, b: 2}).matches({a: 1}));
+			assertFalse(equalTo({a: 1, b: 2}).matches({a: 1, b: 2, c: 3}));
+		});
+
+		it('should match undefined values', function () {
+			var a;
+			var b;
+
+			assertTrue(equalTo(a).matches(b));
+		});
+
+		it('should describe as value', function () {
+			var description = new Description();
+
+			equalTo('a value').describeTo(description);
+
+			assertEquals(description.get(), '"a value"');
+		});
+
 	});
 
-	it('should not match different strings', function () {
-		assertFalse(equalTo('a string').matches('another string'));
+	describe('asMatcher', function () {
+		var asMatcher = IsEqual.asMatcher;
+
+		it('should return matchers unchanged', function () {
+			var aMatcher = new TestMatcher();
+
+			var resultMatcher = asMatcher(aMatcher);
+
+			assertEquals(resultMatcher, aMatcher);
+		});
+
+		it('should wrap values in equalTo matchers', function () {
+			var value = { member: 'a member value' };
+
+			var resultMatcher = asMatcher(value);
+
+			assertTrue(hamjest.isMatcher(resultMatcher), 'Should wrap value');
+			assertTrue(resultMatcher.matches({ member: 'a member value' }));
+		});
 	});
 
-	it('should not match strings and numbers', function () {
-		assertFalse(equalTo('2').matches(2));
-	});
+	describe('acceptingMatcher', function () {
+		var acceptingMatcher = IsEqual.acceptingMatcher;
 
-	it('should match different but equivalent objects', function () {
-		assertTrue(equalTo({a: 1, b: 2}).matches({a: 1, b: 2}));
-	});
+		it('should adapt function with asMatcher: with simple values', function () {
+			var passedValue;
+			var aFunction = function (matcher) {
+				passedValue = matcher;
+			};
 
-	it('should not match unequivalent objects', function () {
-		assertFalse(equalTo({a: 1, b: 2}).matches({a: 1, b: 3}));
-		assertFalse(equalTo({a: 1, b: 2}).matches({a: 1}));
-		assertFalse(equalTo({a: 1, b: 2}).matches({a: 1, b: 2, c: 3}));
-	});
+			var wrappedFunction = acceptingMatcher(aFunction);
+			wrappedFunction('not a matcher');
 
-	it('should match undefined values', function () {
-		var a;
-		var b;
+			assertTrue(hamjest.isMatcher(passedValue));
+		});
 
-		assertTrue(equalTo(a).matches(b));
-	});
+		it('should adapt function with asMatcher: with matcher', function () {
+			var passedValue;
+			var aFunction = function (matcher) {
+				passedValue = matcher;
+			};
+			var aMatcher = new TestMatcher();
 
-	it('should describe as value', function () {
-		var description = new Description();
+			var wrappedFunction = acceptingMatcher(aFunction);
+			wrappedFunction(aMatcher);
 
-		equalTo('a value').describeTo(description);
-
-		assertEquals(description.get(), '"a value"');
+			assertEquals(passedValue, aMatcher);
+		});
 	});
 
 });
