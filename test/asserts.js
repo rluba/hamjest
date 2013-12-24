7b0347ef96fef69ae241063e876123b7fe5e59e9
Raphael Luba <raphael@leanbyte.com>
2013-12-24 07:48:03 +0100

    Use AssertionError for simple asserts (to filter call stack)

--- test/asserts.js
+++ test/asserts.js
@@ -1,23 +1,24 @@
 'use strict';
 
 var _ = require('lodash-node')
+	, AssertionError = require('assertion-error')
 	;
 
 var asserts = {
 	assertTrue: function (value, message) {
 		if (!value) {
-			throw new Error(message);
+			throw new AssertionError(message, {}, asserts.assertTrue);
 		}
 	},
 	assertEquals: function (actual, expected, message) {
 		message = message ? message + ' ' : '';
 		if (!_.isEqual(actual, expected)) {
-			throw new Error(message + 'Expected "' + expected + '" but was "' + actual + '"');
+			throw new AssertionError(message + 'Expected "' + expected + '" but was "' + actual + '"', {}, asserts.assertEquals);
 		}
 	},
 	assertFalse: function (value, message) {
 		if (value) {
-			throw new Error(message);
+			throw new AssertionError(message, {}, asserts.assertFalse);
 		}
 	},
 };
