d3b3fe1055ed788b3f2365899f4d034f64823e3a
Raphael Luba <raphael@leanbyte.com>
2013-12-27 19:54:49 +0100

    Implement delegating matcher „is“

---
+++ test/matchers/IsSpec.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var Is = require('../../lib/matchers/Is')
+	, Description = require('../../lib/Description')
+	, __ = require('../../lib/hamjest')
+	, assertTrue = require('../asserts').assertTrue
+	, assertFalse = require('../asserts').assertFalse
+	;
+
+describe('Is', function () {
+
+	describe('is', function () {
+		var is = Is.is;
+
+		it('should return a matcher', function () {
+
+			var matcher = is(__.equalTo('a value'));
+
+			assertTrue(__.isMatcher(matcher));
+		});
+
+		it('should delegate matching', function () {
+
+			var matcher = is(__.equalTo('expected value'));
+
+			assertTrue(matcher.matches('expected value'));
+			assertFalse(matcher.matches('another value'));
+		});
+
+		it('should expand on inner description', function () {
+			var description = new Description();
+
+			var matcher = is(__.containsString('a value'));
+			matcher.describeTo(description);
+
+			__.assertThat(description.get(), __.equalTo('is a string containing "a value"'));
+		});
+
+		it('should delegate mismatch description', function () {
+			var description = new Description();
+
+			var matcher = is(__.containsString('a value'));
+			matcher.describeMismatch(7, description);
+
+			__.assertThat(description.get(), __.equalTo('was a number (<7>)'));
+		});
+	});
+});
