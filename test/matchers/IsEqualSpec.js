8077530d10f61186c7e838dbc6e508cdd12734a1
Raphael Luba <raphael@leanbyte.com>
2013-12-29 12:37:52 +0100

    Fix: acceptingMatcher did not preserve the inner function’s return value.

--- test/matchers/IsEqualSpec.js
+++ test/matchers/IsEqualSpec.js
@@ -105,27 +105,33 @@ describe('IsEqual', function () {
 
 		it('should adapt function with asMatcher: with simple values', function () {
 			var passedValue;
+			var returnValue = 'a return value';
 			var aFunction = function (matcher) {
 				passedValue = matcher;
+				return returnValue;
 			};
 
 			var wrappedFunction = acceptingMatcher(aFunction);
-			wrappedFunction('not a matcher');
+			var result = wrappedFunction('not a matcher');
 
 			assertTrue(__.isMatcher(passedValue));
+			__.assertThat(result, __.equalTo(returnValue));
 		});
 
 		it('should adapt function with asMatcher: with matcher', function () {
 			var passedValue;
+			var returnValue = 'a return value';
 			var aFunction = function (matcher) {
 				passedValue = matcher;
+				return returnValue;
 			};
 			var aMatcher = new TestMatcher();
 
 			var wrappedFunction = acceptingMatcher(aFunction);
-			wrappedFunction(aMatcher);
+			var result = wrappedFunction(aMatcher);
 
 			assertEquals(passedValue, aMatcher);
+			__.assertThat(result, __.equalTo(returnValue));
 		});
 	});
 });
