58f90d3582cfb539004c1de52d3059ab6110ee12
Raphael Luba <raphael@leanbyte.com>
2013-12-29 15:34:27 +0100

    Implement number matcher

---
+++ test/matchers/IsNumberSpec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var IsNumber = require('../../lib/matchers/IsNumber')
+	, Description = require('../../lib/Description')
+	, __ = require('../../lib/hamjest')
+	, assertTrue = require('../asserts').assertTrue
+	, assertFalse = require('../asserts').assertFalse
+	;
+
+describe('IsNumber', function () {
+
+	describe('number', function () {
+		var number = IsNumber.number;
+		var sut;
+
+		beforeEach(function () {
+			sut = number();
+		});
+
+		it('should return a matcher', function () {
+			assertTrue(__.isMatcher(sut));
+		});
+
+		it('should match any number', function () {
+			assertTrue(sut.matches(7));
+			assertTrue(sut.matches(7.7));
+		});
+
+		it('should not match non-numbers', function () {
+			assertFalse(sut.matches({}));
+			assertFalse(sut.matches([]));
+			assertFalse(sut.matches('a string'));
+		});
+
+		describe('description', function () {
+			var description;
+
+			beforeEach(function () {
+				description = new Description();
+			});
+
+			it('should be nice', function () {
+				var description = new Description();
+
+				sut.describeTo(description);
+
+				__.assertThat(description.get(), __.equalTo('a number'));
+			});
+
+			it('should contain non-number values', function () {
+				var description = new Description();
+
+				sut.describeMismatch({an: 'object'}, description);
+
+				__.assertThat(description.get(), __.equalTo('was a object ({"an":"object"})'));
+			});
+		});
+	});
+});
