676cc5fcfcc148232b45c6d71899712a46482ee1
Raphael Luba <raphael@leanbyte.com>
2013-12-29 15:25:11 +0100

    Implement bool matcher

---
+++ test/matchers/IsBooleanSpec.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var IsBoolean = require('../../lib/matchers/IsBoolean')
+	, Description = require('../../lib/Description')
+	, __ = require('../../lib/hamjest')
+	, assertTrue = require('../asserts').assertTrue
+	, assertFalse = require('../asserts').assertFalse
+	;
+
+describe('IsBoolean', function () {
+
+	describe('bool', function () {
+		var bool = IsBoolean.bool;
+		var sut;
+
+		beforeEach(function () {
+			sut = bool();
+		});
+
+		it('should return a matcher', function () {
+			assertTrue(__.isMatcher(sut));
+		});
+
+		// requires "throws" matcher
+		it('should throw for non-bool arguments');
+
+		it('should match any bool', function () {
+			assertTrue(sut.matches(true));
+			assertTrue(sut.matches(false));
+		});
+
+		it('should not match non-bools', function () {
+			assertFalse(sut.matches(7));
+			assertFalse(sut.matches({}));
+			assertFalse(sut.matches('a string'));
+		});
+
+		describe('description', function () {
+			var description;
+
+			beforeEach(function () {
+				description = new Description();
+			});
+
+			it('should be nice', function () {
+				var description = new Description();
+
+				sut.describeTo(description);
+
+				__.assertThat(description.get(), __.equalTo('a boolean'));
+			});
+
+			it('should contain non-bool values', function () {
+				var description = new Description();
+
+				sut.describeMismatch({an: 'object'}, description);
+
+				__.assertThat(description.get(), __.equalTo('was a object ({"an":"object"})'));
+			});
+		});
+	});
+});
---
+++ test/matchers/IsDateSpec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var IsDate = require('../../lib/matchers/IsDate')
+	, Description = require('../../lib/Description')
+	, __ = require('../../lib/hamjest')
+	, assertTrue = require('../asserts').assertTrue
+	, assertFalse = require('../asserts').assertFalse
+	;
+
+describe('IsDate', function () {
+
+	describe('date', function () {
+		var date = IsDate.date;
+		var sut;
+
+		beforeEach(function () {
+			sut = date();
+		});
+
+		it('should return a matcher', function () {
+			assertTrue(__.isMatcher(sut));
+		});
+
+		it('should match any date', function () {
+			assertTrue(sut.matches(new Date(2017, 14, 2)));
+			assertTrue(sut.matches(new Date('2007-13-5')));
+		});
+
+		it('should not match non-dates', function () {
+			assertFalse(sut.matches(7));
+			assertFalse(sut.matches({}));
+			assertFalse(sut.matches('a string'));
+		});
+
+		describe('description', function () {
+			var description;
+
+			beforeEach(function () {
+				description = new Description();
+			});
+
+			it('should be nice', function () {
+				var description = new Description();
+
+				sut.describeTo(description);
+
+				__.assertThat(description.get(), __.equalTo('a date'));
+			});
+
+			it('should contain non-date values', function () {
+				var description = new Description();
+
+				sut.describeMismatch({an: 'object'}, description);
+
+				__.assertThat(description.get(), __.equalTo('was a object ({"an":"object"})'));
+			});
+		});
+	});
+});
