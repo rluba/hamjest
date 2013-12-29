7110e8e8aaca9083cf3d4316a6754a72367864d8
Raphael Luba <raphael@leanbyte.com>
2013-12-29 15:32:15 +0100

    Implement func matcher

---
+++ test/matchers/IsFunctionSpec.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var IsFunction = require('../../lib/matchers/IsFunction')
+	, Description = require('../../lib/Description')
+	, __ = require('../../lib/hamjest')
+	, assertTrue = require('../asserts').assertTrue
+	, assertFalse = require('../asserts').assertFalse
+	;
+
+describe('IsFunction', function () {
+
+	describe('func', function () {
+		var func = IsFunction.func;
+		var sut;
+
+		beforeEach(function () {
+			sut = func();
+		});
+
+		it('should return a matcher', function () {
+			assertTrue(__.isMatcher(sut));
+		});
+
+		it('should match any func', function () {
+			function namedFunction() {
+
+			}
+			assertTrue(sut.matches(function () {}));
+			assertTrue(sut.matches(namedFunction));
+			assertTrue(sut.matches(String));
+			assertTrue(sut.matches(Description));
+		});
+
+		it('should not match non-funcs', function () {
+			assertFalse(sut.matches(7));
+			assertFalse(sut.matches({}));
+			assertFalse(sut.matches('a string'));
+		});
+
+		describe('description', function () {
+			var description;
+
+			beforeEach(function () {
+				description = new Description();
+			});
+
+			it('should be nice', function () {
+				var description = new Description();
+
+				sut.describeTo(description);
+
+				__.assertThat(description.get(), __.equalTo('a function'));
+			});
+
+			it('should contain non-func values', function () {
+				var description = new Description();
+
+				sut.describeMismatch({an: 'object'}, description);
+
+				__.assertThat(description.get(), __.equalTo('was a object ({"an":"object"})'));
+			});
+		});
+	});
+});
