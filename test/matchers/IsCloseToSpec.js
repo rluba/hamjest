fb98b91b946846519815b09e46f507eb6afcf005
Raphael Luba <raphael@leanbyte.com>
2013-12-30 10:23:51 +0100

    Implement closeTo number matcher

---
+++ test/matchers/IsCloseToSpec.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var AssertionError = require('assertion-error')
+	, IsCloseTo = require('../../lib/matchers/IsCloseTo')
+	, Description = require('../../lib/Description')
+	, __ = require('../../lib/hamjest')
+	;
+
+describe('IsCloseTo', function () {
+
+	describe('closeTo', function () {
+		var closeTo = IsCloseTo.closeTo;
+		var sut;
+
+		beforeEach(function () {
+			sut = closeTo(7, 0.5);
+		});
+
+		it('should throw for non-number arguments', function () {
+			__.assertThat(function () {
+				closeTo('5', 0.5);
+			}, __.throws(AssertionError));
+
+			__.assertThat(function () {
+				closeTo(1, '0.2');
+			}, __.throws(AssertionError));
+
+			__.assertThat(function () {
+				closeTo(1);
+			}, __.throws(AssertionError));
+		});
+
+		it('should throw for missing delta', function () {
+			__.assertThat(function () {
+				closeTo(1);
+			}, __.throws(AssertionError));
+		});
+
+		it('should match numbers correctly', function () {
+			__.assertThat(sut.matches(6.5), __.is(true));
+			__.assertThat(sut.matches(6.50001), __.is(true));
+			__.assertThat(sut.matches(7), __.is(true));
+			__.assertThat(sut.matches(7.5), __.is(true));
+
+			__.assertThat(sut.matches(6.49), __.is(false));
+			__.assertThat(sut.matches(7.51), __.is(false));
+		});
+
+		it('should not match non-numbers', function () {
+			__.assertThat(sut.matches(), __.is(false));
+			__.assertThat(sut.matches('7'), __.is(false));
+		});
+
+		describe('description', function () {
+			var description;
+
+			beforeEach(function () {
+				description = new Description();
+			});
+
+			it('should contain value and delta', function () {
+				var description = new Description();
+
+				sut.describeTo(description);
+
+				__.assertThat(description.get(), __.equalTo('a number within <0.5> of <7>'));
+			});
+
+			it.skip('should contain mismatched number and real delta', function () {
+				var description = new Description();
+
+				sut.describeMismatch(6.4, description);
+
+				__.assertThat(description.get(), __.equalTo('<6.4> differed by <0.6>'));
+			});
+
+			it('should contain non-number values', function () {
+				var description = new Description();
+
+				sut.describeMismatch({an: 'object'}, description);
+
+				__.assertThat(description.get(), __.equalTo('was a object ({"an":"object"})'));
+			});
+		});
+	});
+});
