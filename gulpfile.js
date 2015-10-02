42722487ea0798de3092ec93f2377b207f898761
Raphael Luba <raphael@leanbyte.com>
2015-10-02 23:13:33 +0200

    Replace Grunt with Gulp
    Always bundle dependencies into builds

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserify = require('browserify');
+var del = require('del');
+var sourceStream = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+var jsFiles = [
+	'*.js',
+	'lib/**/*.js',
+	'test/**/*.js'
+];
+
+gulp.task('default', ['lint', 'build']);
+
+gulp.task('lint', function () {
+	return gulp.src(jsFiles)
+		.pipe($.jshint())
+		.pipe($.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('test', function () {
+	return gulp.src('test/**/*Spec.js', {read: false})
+		.pipe($.mocha({
+			reporter: 'spec'
+		}));
+});
+
+gulp.task('build', ['test'], function () {
+	var b = browserify({
+		entries: './hamjest.js'
+	});
+
+	return b.bundle()
+		.pipe(sourceStream('hamjest.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('./dist'))
+		.pipe($.uglify())
+		.pipe($.rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('clean', function (done) {
+	del('./dist', done);
+});
+
+gulp.task('dev', function () {
+    gulp.watch(jsFiles, ['lint', 'test']);
+});
