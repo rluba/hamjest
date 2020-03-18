ca699b1c393292d080480a43dc98064ce498619a
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2020-03-18 16:44:42 +0100

    Add "is" and "not" matcher

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,10 +1,71 @@
 declare module "hamjest" {
   type TODO = any
+  type Matcher = TODO;
   type Value = any
 
-  export function assertThat(actual: Value, matcher: TODO): void
-  export function assertThat(reason: string, actual: Value, matcher: TODO): void
+  export function assertThat(actual: Value, matcher: Matcher): void
+  export function assertThat(reason: string, actual: Value, matcher: Matcher): void
 
+
+
+  // AllOf.js
+  // AnyOf.js
+  // DateComparisonMatcher.js
+  // Every.js
+  // FeatureMatcher.js
+  // Is.js
+  export function is(matcher: Matcher): void
+
+  // IsAnything.js
+  // IsArray.js
+  // IsArrayContaining.js
+  // IsArrayContainingInAnyOrder.js
+  // IsArrayOrderedBy.js
+  // IsArrayWithItem.js
+  // IsArrayWithItems.js
+  // IsBoolean.js
+  // IsCloseTo.js
+  // IsDate.js
+  // IsDefined.js
+  // IsEqual.js
   export function equalTo(expectedValue: Value): void
+
+  // IsFulfilled.js
+  // IsFunction.js
+  // IsFunctionThrowing.js
+  // IsInstanceOf.js
+  // IsNot.js
+  export function not(matcher: Matcher): void
+
+  // IsNumber.js
+  // IsObject.js
+  // IsObjectWithProperties.js
+  // IsPromise.js
+  // IsRegExp.js
+  // IsRejected.js
+  // IsSame.js
+  // IsString.js
+  // IsStringMatching.js
+  // Matcher.js
+  // NumberComparisonMatcher.js
+  // SubstringMatcher.js
+  // TypeSafeMatcher.js
+  // failsToMatch.js
+  // falsy.js
+  // hasDescription.js
+  // hasExactlyOneItem.js
+  // hasSize.js
+  // inRange.js
+  // isEmpty.js
+  // matches.js
+  // promiseAgnostic.js
+  // returns.js
+  // truthy.js
+  // typedError.js
+
+
+
+
+
 
 }
--- @types/typeTests.ts
+++ @types/typeTests.ts
@@ -2,3 +2,7 @@ import * as _ from 'hamjest'
 
 _.assertThat(1, _.equalTo(1))
 _.assertThat('Reason', 1, _.equalTo(1))
+
+_.assertThat(1, _.is(_.equalTo(1)))
+
+_.assertThat(1, _.not(_.equalTo(1)))
