256c27c1225ca6fae1f6f482cd3ac1cbc9e9de44
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2020-03-18 17:01:18 +0100

    Add some array helpers

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -7,68 +7,97 @@ declare module "hamjest" {
   export function assertThat(reason: string, actual: Value, matcher: Matcher): void
 
 
-
-  // AllOf.js
-  // AnyOf.js
-  // DateComparisonMatcher.js
-  // Every.js
-  // FeatureMatcher.js
-  // Is.js
-  export function is(matcher: Matcher): void
-
-  // IsAnything.js
+  // anything: require('./matchers/IsAnything').anything,
   export function anything(): void
 
-  // IsArray.js
-  // IsArrayContaining.js
-  // IsArrayContainingInAnyOrder.js
-  // IsArrayOrderedBy.js
-  // IsArrayWithItem.js
-  // IsArrayWithItems.js
-  // IsBoolean.js
-  // IsCloseTo.js
-  // IsDate.js
-  // IsDefined.js
-  // IsEqual.js
+  // strictlyEqualTo: require('./matchers/IsSame').strictlyEqualTo,
   export function equalTo(expectedValue: Value): void
 
-  // IsFulfilled.js
-  // IsFunction.js
-  // IsFunctionThrowing.js
-  // IsInstanceOf.js
-  // IsNot.js
+  // is: require('./matchers/Is').is,
+  export function is(matcher: Matcher): void
+
+  // not: require('./matchers/IsNot').not,
   export function not(matcher: Matcher): void
 
-  // IsNumber.js
-  // IsObject.js
-  // IsObjectWithProperties.js
-  // IsPromise.js
-  // IsRegExp.js
-  // IsRejected.js
-  // IsSame.js
-  // IsString.js
-  // IsStringMatching.js
-  // Matcher.js
-  // NumberComparisonMatcher.js
-  // SubstringMatcher.js
-  // TypeSafeMatcher.js
-  // failsToMatch.js
-  // falsy.js
-  export function falsy(): void
+  // equalTo: IsEqual.equalTo,
+  export function equalTo(expectedValue: Value): void
 
-  // hasDescription.js
-  // hasExactlyOneItem.js
-  // hasSize.js
-  export function hasSize(size: number)
-
-  // inRange.js
-  // isEmpty.js
-  // matches.js
-  // promiseAgnostic.js
-  // returns.js
-  // truthy.js
+  // truthy: require('./matchers/truthy'),
   export function truthy(): void
 
-  // typedError.js
+  // falsy: require('./matchers/falsy'),
+  export function falsy(): void
+
+  // falsey: require('./matchers/falsy'),
+  export function falsey(): void
+
+	// defined: require('./matchers/IsDefined').defined,
+	// undefined: require('./matchers/IsDefined').undefined,
+	// undef: require('./matchers/IsDefined').undefined,
+	// instanceOf: require('./matchers/IsInstanceOf').instanceOf,
+  // array: require('./matchers/IsArray').array,
+  export function array(): void
+
+	// bool: require('./matchers/IsBoolean').bool,
+	// boolean: require('./matchers/IsBoolean').bool,
+	// date: require('./matchers/IsDate').date,
+	// func: require('./matchers/IsFunction').func,
+	// number: require('./matchers/IsNumber').number,
+	// object: require('./matchers/IsObject').object,
+	// regExp: require('./matchers/IsRegExp').regExp,
+	// string: require('./matchers/IsString').string,
+	// containsString: SubstringMatcher.containsString,
+	// containsStrings: SubstringMatcher.containsStrings,
+	// startsWith: SubstringMatcher.startsWith,
+	// endsWith: SubstringMatcher.endsWith,
+	// matchesPattern: require('./matchers/IsStringMatching').matchesPattern,
+	// matches: require('./matchers/matches'),
+	// failsToMatch: require('./matchers/failsToMatch'),
+	// hasDescription: require('./matchers/hasDescription'),
+	// lessThan: NumberComparisonMatcher.lessThan,
+	// lessThanOrEqualTo: NumberComparisonMatcher.lessThanOrEqualTo,
+	// greaterThan: NumberComparisonMatcher.greaterThan,
+	// greaterThanOrEqualTo: NumberComparisonMatcher.greaterThanOrEqualTo,
+  // inRange: require('./matchers/inRange'),
+  export function inRange(upperBound: number)
+  export function inRange(lowerBound: number, upperBound: number)
+
+	// after: DateComparisonMatcher.after,
+	// afterOrEqualTo: DateComparisonMatcher.afterOrEqualTo,
+	// before: DateComparisonMatcher.before,
+	// beforeOrEqualTo: DateComparisonMatcher.beforeOrEqualTo,
+	// closeTo: require('./matchers/IsCloseTo').closeTo,
+	// allOf: require('./matchers/AllOf').allOf,
+	// anyOf: require('./matchers/AnyOf').anyOf,
+	// everyItem: require('./matchers/Every').everyItem,
+	// hasItem: require('./matchers/IsArrayWithItem').hasItem,
+	// hasItems: require('./matchers/IsArrayWithItems').hasItems,
+  // hasExactlyOneItem: require('./matchers/hasExactlyOneItem'),
+  export function hasExactlyOneItem()
+
+	// contains: require('./matchers/IsArrayContaining').contains,
+	// containsInAnyOrder: require('./matchers/IsArrayContainingInAnyOrder').containsInAnyOrder,
+	// orderedBy: require('./matchers/IsArrayOrderedBy').orderedBy,
+  // hasSize: require('./matchers/hasSize'),
+  export function hasSize(size: number)
+
+  // isEmpty: require('./matchers/isEmpty'),
+  export function isEmpty()
+
+	// empty: require('./matchers/isEmpty'),
+	// hasProperties: require('./matchers/IsObjectWithProperties').hasProperties,
+	// hasProperty: require('./matchers/IsObjectWithProperties').hasProperty,
+	// throws: require('./matchers/IsFunctionThrowing').throws,
+	// returns: require('./matchers/returns'),
+	// typedError: require('./matchers/typedError'),
+	// promise: require('./matchers/IsPromise').promise,
+	// fulfilled: require('./matchers/IsFulfilled').fulfilled,
+	// isFulfilledWith: require('./matchers/IsFulfilled').isFulfilledWith,
+	// willBe: require('./matchers/IsFulfilled').isFulfilledWith,
+	// rejected: require('./matchers/IsRejected').rejected,
+	// isRejectedWith: require('./matchers/IsRejected').isRejectedWith,
+	// promiseAllOf: require('./matchers/AllOf').allOf
+
+
 
 }
--- @types/typeTests.ts
+++ @types/typeTests.ts
@@ -10,10 +10,20 @@ _.assertThat(2, _.is(_.not(_.equalTo(1))))
 
 
 _.assertThat(true, _.truthy())
+
 _.assertThat(false, _.falsy())
+_.assertThat(false, _.falsey())
 
 _.assertThat(false, _.anything())
 
 
 
 _.assertThat([1,2,3], _.hasSize(3))
+
+_.assertThat([], _.isEmpty())
+
+_.assertThat(1, _.inRange(2))
+_.assertThat(1, _.inRange(0, 2))
+
+_.assertThat([1], _.hasExactlyOneItem())
+_.assertThat([1], _.array())
